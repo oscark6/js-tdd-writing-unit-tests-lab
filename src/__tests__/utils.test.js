246d91d07df975dbc6ad0bc0f35b99575a4e5920
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-05-27 08:35:47 +0300

    done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,18 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+test('returns false for a non-palindrome', () => {
+    expect(isPalindrome('hello')).toBe(false);
+  });
+  
+  test('returns true for a palindrome with mixed case', () => {
+    expect(isPalindrome('Racecar')).toBe(true);
+  });
+  
+  test('throws an error for non-alphabetic characters', () => {
+    expect(() => isPalindrome('hello123')).toThrow('Input must only contain alphabetic characters');
+  });
+  
+  test('throws an error for non-string input', () => {
+    expect(() => isPalindrome(123)).toThrow('Input must be a string');
+  });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,17 @@
 // Your code here
+export function isPalindrome(word) {
+    if (typeof word !== 'string') {
+      throw new Error('Input must be a string');
+    }
+  
+    if (!/^[A-Za-z]*$/.test(word)) {
+      throw new Error('Input must only contain alphabetic characters');
+    }
+  
+    if (word === '') {
+      return true;
+    }
+  
+    const reversedWord = word.split('').reverse().join('');
+    return word.toLowerCase() === reversedWord.toLowerCase();
+  }
